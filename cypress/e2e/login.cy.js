e0ff3e7cb0481fe2668b4c4f15ec347de06ad7c6
Tiago Schmitt <tiagoschmitt@proton.me>
2022-09-20 14:02:21 -0300

    Aula de Arquivos de Dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login' , () =>{
 
@@ -18,6 +19,24 @@ context('Funcionalidade Login' , () =>{
         cy.get('.page-title').should('contain' , 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, teste_aluno20 (não é teste_aluno20? Sair)')
     })
+    
+    it('Deve fazer login com sucesso - usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain' , 'Minha conta')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados =>{
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()
+    
+            cy.get('.page-title').should('contain' , 'Minha conta')
+        })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir usuário' , () =>{
         cy.get('#username').type('ebac@teste.com')
